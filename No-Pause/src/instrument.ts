7a93f01774bd50835cb459ff1ebb8e91eba26e6c
Bobur Karimov <viseeroy@gmail.com>
2026-05-15 17:57:37 +0900

    Hardcode Sentry DSN

--- No-Pause/src/instrument.ts
+++ No-Pause/src/instrument.ts
@@ -1,6 +1,6 @@
 import * as Sentry from "@sentry/react";
 
-const sentryDsn = import.meta.env.VITE_SENTRY_DSN;
+const sentryDsn = "https://8ce2b4b133030b9a186a05ceabee5417@o4511392880590848.ingest.de.sentry.io/4511392912375888";
 
 if (sentryDsn) {
   Sentry.init({
